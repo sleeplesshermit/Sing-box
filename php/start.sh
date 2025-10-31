18f197f29d1c20865093acd2e8f085d26458c914
sleeplesshermit <jerry.liqian@gmail.com>
2025-10-31 10:37:52 +0800

    Update UUID and ARGO configurations in start.sh

--- php/start.sh
+++ php/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
 
-export UUID=${UUID:-'fdeeda45-0a8e-4570-bcc6-d68c995f5830'} # 如开启哪吒v1,不同的平台需要改一下，否则会覆盖
+export UUID=${UUID:-'eca2d944-ff45-442f-bde2-3ddc2d084d7b'} # 如开启哪吒v1,不同的平台需要改一下，否则会覆盖
 export NEZHA_SERVER=${NEZHA_SERVER:-''}       # v1哪吒填写形式：nezha.abc.com:8008,v0哪吒填写形式：nezha.abc.com
 export NEZHA_PORT=${NEZHA_PORT:-''}           # v1哪吒必须留空,v0哪吒agent端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
 export NEZHA_KEY=${NEZHA_KEY:-''}             # 哪吒v0-agent密钥或v1的NZ_CLIENT_SECRET
-export ARGO_DOMAIN=${ARGO_DOMAIN:-''}         # 固定隧道域名,留空即启用临时隧道
-export ARGO_AUTH=${ARGO_AUTH:-''}             # 固定隧道token或json,留空即启用临时隧道, json获取：https://json.zone.id
+export ARGO_DOMAIN=${ARGO_DOMAIN:-'cf-tunnel-roaming-1.kingslanding.eu.org'}         # 固定隧道域名,留空即启用临时隧道
+export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiNTZjMGEwMjEyMmY4NDk3MjQ5Y2QwZjE3M2RiYmMwZDIiLCJ0IjoiZjgwZjhiODMtZjliNC00MGNmLTgzNzItNTI4NmY5NDM0ZjkwIiwicyI6IlpXUXdOV0V3TVRJdE16STVNQzAwTmpVMkxUbGtOVEl0WldRME1qRXpNMk0wTkdRNSJ9'}             # 固定隧道token或json,留空即启用临时隧道, json获取：https://json.zone.id
 export CFIP=${CFIP:-'time.is'}                # argo节点优选域名或优选ip
 export CFPORT=${CFPORT:-'443'}                # argo节点端口 
 export NAME=${NAME:-'sbx'}                    # 节点名称  
